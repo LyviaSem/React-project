3f8e61e4925d06f836c91a7ee739fd14bd62949e
lyvia <lyviasemaoune@gmail.com>
2023-02-02 15:32:34 +0100

    Affichage categorie et produit

--- mony/src/mocks/products.js
+++ mony/src/mocks/products.js
@@ -1,107 +1,136 @@
 export const PRODUCTS = [
     {
-        id: 0,
-        categoryTitle: "salade",
-        specificRegime: "meat",
-        children: [
-            {
-                id: 1,
-                category: "salade",
-                title: "Salade César",
-                description: "Ceci est une chaise",
-                allergenes:"",
-                specificRegime:"",
-                price: 50
-            },
-            {
-                id: 2,
-                category: "salade",
-                title: "Salade Lyonnaise",
-                description: "Ceci est une chaise",
-                allergenes:"",
-                specificRegime:"",
-                price: 50
-            },
-            {
-                id: 3,
-                category: "salade",
-                title: "Salade Savoyarde",
-                description: "Ceci est une chaise",
-                allergenes:"",
-                specificRegime:"",
-                price: 50
-            }
-        ]
+       id: 1,
+       category:"Burger",
+       children: [
+        
+       ]
     },
-
-    {
-        id: 1,
-        categoryTitle: "salade",
-        specificRegime: "fish",
-        children: [
-            {
-                id: 1,
-                category: "salade",
-                title: "Salade Océane",
-                description: "Ceci est une chaise",
-                allergenes:"",
-                specificRegime:"",
-                price: 50
-            },
-            {
-                id: 2,
-                category: "salade",
-                title: "Salade Norvégiene",
-                description: "Ceci est une chaise",
-                allergenes:"",
-                specificRegime:"",
-                price: 50
-            },
-            {
-                id: 3,
-                category: "salade",
-                title: "Salade Niçoise",
-                description: "Ceci est une chaise",
-                allergenes:"",
-                specificRegime:"",
-                price: 50
-            }
-        ]
-    },
-
     {
         id: 2,
-        categoryTitle: "salade",
-        specificRegime: "veggie",
+        category: "Pizza",
+        children: [
+
+        ]
+    },
+    {
+        id: 3,
+        category: "Salade",
         children: [
             {
                 id: 1,
-                category: "salade",
-                title: "Salade Greque",
-                description: "Ceci est une chaise",
-                allergenes:"",
-                specificRegime:"",
-                price: 50
+                specificRegime: "meat",
+                children: [
+                    {
+                        id: 1,
+                        category: "salade",
+                        title: "Salade César",
+                        description: "Ceci est une chaise",
+                        allergenes:"",
+                        specificRegime:"",
+                        price: 50
+                    },
+                    {
+                        id: 2,
+                        category: "salade",
+                        title: "Salade Lyonnaise",
+                        description: "Ceci est une chaise",
+                        allergenes:"",
+                        specificRegime:"",
+                        price: 50
+                    },
+                    {
+                        id: 3,
+                        category: "salade",
+                        title: "Salade Savoyarde",
+                        description: "Ceci est une chaise",
+                        allergenes:"",
+                        specificRegime:"",
+                        price: 50
+                    }
+                ]
             },
             {
                 id: 2,
-                category: "salade",
-                title: "Chèvre Chaud",
-                description: "Ceci est une chaise",
-                allergenes:"",
-                specificRegime:"",
-                price: 50
+                specificRegime: "fish",
+                children: [
+                    {
+                        id: 1,
+                        category: "salade",
+                        title: "Salade Océane",
+                        description: "Ceci est une chaise",
+                        allergenes:"",
+                        specificRegime:"",
+                        price: 50
+                    },
+                    {
+                        id: 2,
+                        category: "salade",
+                        title: "Salade Norvégiene",
+                        description: "Ceci est une chaise",
+                        allergenes:"",
+                        specificRegime:"",
+                        price: 50
+                    },
+                    {
+                        id: 3,
+                        category: "salade",
+                        title: "Salade Niçoise",
+                        description: "Ceci est une chaise",
+                        allergenes:"",
+                        specificRegime:"",
+                        price: 50
+                    }
+                ]
             },
             {
                 id: 3,
-                category: "salade",
-                title: "Salade Arc-en-ciel",
-                description: "Ceci est une chaise",
-                allergenes:"",
-                specificRegime:"",
-                price: 50
+                specificRegime: "veggie",
+                children: [
+                    {
+                        id: 1,
+                        category: "salade",
+                        title: "Salade Greque",
+                        description: "Ceci est une chaise",
+                        allergenes:"",
+                        specificRegime:"",
+                        price: 50
+                    },
+                    {
+                        id: 2,
+                        category: "salade",
+                        title: "Chèvre Chaud",
+                        description: "Ceci est une chaise",
+                        allergenes:"",
+                        specificRegime:"",
+                        price: 50
+                    },
+                    {
+                        id: 3,
+                        category: "salade",
+                        title: "Salade Arc-en-ciel",
+                        description: "Ceci est une chaise",
+                        allergenes:"",
+                        specificRegime:"",
+                        price: 50
+                    }
+                ]
             }
         ]
     },
+    {
+        id: 4,
+        category: "Pâtes",
+        children: [
+
+        ]
+    },
+    {
+        id: 5,
+        category: "Desserts",
+        children: [
+
+        ]
+    }
 
 ]
--- mony/src/pages/home/home.jsx
+++ mony/src/pages/home/home.jsx
@@ -1,7 +1,19 @@
-const Home = () => {
-    return(
+import { getProduct } from "../../Services/Products.service"
+import { useLoaderData } from "react-router-dom";
+import {Link} from "react-router-dom";
+
+
+
+const Home = () =>{
+    const products = useLoaderData();
+    const {category, description, price, children} = useLoaderData();
+
+    return (
         <main>
-            <h1>Home fonctionne</h1>
+            <h1>Nos Menus</h1>
+             <ul>
+                {products.map((product) => <li key={product.id}> {product.category}</li>)}
+            </ul> 
         </main>
     )
 }
